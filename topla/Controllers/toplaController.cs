88b12096555a0b7603b0aea87f2dfcfea9822fad
prederyo <ifkaraca14@gmail.com>
2024-07-09 18:02:47 +0300

    5 farklı değer yazıldı

---
+++ topla/Controllers/toplaController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+
+namespace topla.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class toplaController : ControllerBase
+    {
+        [HttpPost("islem1")]
+        public IActionResult islem1(int x, int y)
+        {
+            int topla = x + y;
+            return Ok(topla);
+        }
+
+        [HttpGet("islem2")]
+        public int Islem2 (int x , int y)
+        {
+            return x + y;
+        }
+
+        [HttpPost("islem3")]
+        public IActionResult islem3([FromBody] Sayi sayi)
+        {
+            int toplam = sayi.A + sayi.B;
+            return Ok(toplam);
+        }
+
+        public class Sayi
+        {
+            public int A { get; set; }
+            public int B { get; set; }
+        }
+
+        [HttpPost("islem4")]
+        public JsonResult islem4(int x, int y)
+        {
+            int topla = x + y;
+            return new JsonResult(topla);
+        }
+
+        [HttpPost("islem5")]
+        public ContentResult Islem5(int x, int y)
+        {
+            int topla = x + y;
+            return new ContentResult
+            {
+                Content = topla.ToString(),
+            };
+        }
+    }
+}
